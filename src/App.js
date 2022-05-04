be929ac8a9bfd3aa0428bf1eba978f991bb77bce
Brandon Jones <104112201+brandonjones8643@users.noreply.github.com>
2022-05-03 20:39:37 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <>
+      <h1>Brandon Jones</h1>
+      <p>-Javascript Developer</p>
+      <h3>Email:</h3>
+      <p>BrandonJones8643@gmail.com</p>
+      <h3>GitHub:</h3>
+      <a href="https://github.com/brandonjones8643">
+        https://github.com/brandonjones8643
+      </a>
+      <h3>Fiver:</h3>
+      <a href="https://www.fiverr.com/s2/57c3564e95">
+        https://www.fiverr.com/s2/57c3564e95
+      </a>
+      <h3>Technologies:</h3>
+      <ul>
+        <li>ReactJS</li>
+        <li>Javascript</li>
+        <li>Html</li>
+        <li>CSS</li>
+        <li>NodeJS</li>
+        <li>React Native</li>
+        <li>ElectronJS</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
