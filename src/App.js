f9bf1eec0935aa52135e958be586d85941820034
Brandon Jones <104112201+brandonjones8643@users.noreply.github.com>
2022-05-08 22:00:21 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 function App() {
   return (
-    <>
-      <h1>Brandon Jones</h1>
-      <p>-Javascript Developer</p>
+    <div className="app">
+      <div className="title-and-image-container">
+        <div className="title-and-image">
+          <img src="img.jpg" alt="profileimage" />
+          <h2>Brandon Jones</h2>
+        </div>
+      </div>
+      <p className="description">Javascript Developer</p>
       <h3>Email:</h3>
       <p>BrandonJones8643@gmail.com</p>
       <h3>GitHub:</h3>
-      <a href="https://github.com/brandonjones8643">
-        https://github.com/brandonjones8643
-      </a>
+      <a href="https://github.com/brandonjones8643">Link to GitHub</a>
       <h3>Fiver:</h3>
-      <a href="https://www.fiverr.com/s2/57c3564e95">
-        https://www.fiverr.com/s2/57c3564e95
-      </a>
+      <a href="https://www.fiverr.com/s2/57c3564e95">Link to Fiverr</a>
       <h3>Technologies:</h3>
       <ul>
         <li>ReactJS</li>
@@ -23,7 +24,7 @@ function App() {
         <li>React Native</li>
         <li>ElectronJS</li>
       </ul>
-    </>
+    </div>
   );
 }
 
